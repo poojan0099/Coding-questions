72522896ced237c52b0fef73d8c7377e43cbab46
poojan0099 <48233985+poojan0099@users.noreply.github.com>
2022-08-15 12:20:21 +0530

    Add files via upload

---
+++ Swap_numbers.java
@@ -0,0 +1,55 @@
+package practice_programs;
+
+public class Swap_numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Methos 1 uing temp variable
+		
+		int temp;
+		int a = 10, b =20;
+		System.out.println("Befor Swapping a =  "+a+", b = "+b);
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("After Swapping a = "+a+", b = "+b);
+		
+		System.out.println();
+		
+		//Method 2 using addition subtraction
+		int x = 100, y = 200;
+		System.out.println("Berfore Swapping x = "+x+", y = "+y);
+		
+		x = x + y;
+		y = x - y;
+		x = x - y;
+			
+		System.out.println("After Swapping x = "+x+", y = "+y);
+		System.out.println();
+		
+		//Method 3 using division and multiplication method
+		int p = 5, q = 6;
+		System.out.println("Berfore Swapping p = "+p+", q = "+q);
+		
+		p = p * q;
+		q = p / q;
+		p = p / q;
+
+		System.out.println("After Swapping x = "+p+", y = "+q);
+		System.out.println();
+		//Method 4 using XOR operation
+		
+		int m = 88, n = 99;
+		System.out.println("Before Swapping m = "+m+", n = "+n);
+		
+		m = m ^ n;
+		n = m ^ n;
+		m = m ^ n;
+		
+		System.out.println("After Swapping m = "+m+", n = "+n);
+		
+		
+	}
+
+}
